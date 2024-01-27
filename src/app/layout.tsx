0f4415d6a59fff50a9b17fffa6635b15afe2f0fb
Prasanth <santhtce@gmail.com>
2024-01-26 20:11:07 -0500

    Create barebore project with NextJs + MUI + TypeScript + Auth0

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import * as React from 'react'
+import { UserProvider } from '@auth0/nextjs-auth0/client'
+
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import getTheme from '@/theme'
+import { ThemeToggleContext } from '@/components/ThemeToggle/ThemeToggleContext'
+import { PaletteMode } from '@mui/material'
+
+export default function RootLayout(props: { children: React.ReactNode }) {
+  const [mode, setMode] = React.useState<PaletteMode>('light')
+
+  const paletteMode = React.useMemo(
+    () => ({
+      togglePaletteMode: () => {
+        setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'))
+      },
+    }),
+    [],
+  )
+
+  const theme = React.useMemo(() => getTheme(mode), [mode])
+
+  return (
+    <html lang='en'>
+      <body>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <UserProvider>
+            <ThemeToggleContext.Provider value={paletteMode}>
+              <ThemeProvider theme={theme}>
+                <CssBaseline />
+                {props.children}
+              </ThemeProvider>
+            </ThemeToggleContext.Provider>
+          </UserProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import NextLink from "next/link";
+import ProTip from "@/components/ProTip";
+import Copyright from "@/components/Copyright";
+import { ThemeToggle } from "@/components/ThemeToggle/ThemeToggle";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { getSession } from "@auth0/nextjs-auth0";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getSession();
+
+  if (session?.user) {
+    redirect("/dashboard");
+  }
+
+  return (
+    <Container maxWidth="lg">
+      <Box display="flex" justifyContent="space-between">
+        <ThemeToggle></ThemeToggle>
+        <a href="/api/auth/login">Log In</a>
+      </Box>
+      <Box
+        sx={{
+          my: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          Material UI - Next.js App Router example in TypeScript
+        </Typography>
+        <Link href="/about" color="secondary" component={NextLink}>
+          Go to the about page
+        </Link>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
